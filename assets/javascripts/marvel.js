8978601901b26afd0b100c900a7c863ecfb7efa1
Ruben <rubaco01@yahoo.com>
2017-08-29 15:34:27 -0700

    easy complete

--- assets/javascripts/marvel.js
+++ assets/javascripts/marvel.js
@@ -3,65 +3,111 @@
 
     $(function(){
       var character_id;
+      let getStringPageLoad;
       let getString;
       let getLocation;
 
+      //variables at load
+      let loadCharactersName;
+      let loadCharactersPath
+      let loadCharactersExtension
 
+      //variables at search
+      let searchCharactersName;
+      let searchCharactersPath
+      let searchCharactersExtension
 
-//click functionality
-                 $("#find").click(function(){
+      getStringPageLoad = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=8e8d27528df8e1068e2773e3a7161575&hash=412852e32d04be7d8218808df40d53f1";
 
-                   let inputNameStartsWith = $('#search').val();
-                   getString = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=8e8d27528df8e1068e2773e3a7161575&hash=412852e32d04be7d8218808df40d53f1&nameStartsWith="+inputNameStartsWith;
-                   console.log(getString)
+      function initialLoadCharacters(){
+        $("#charactersTable").html("");
+        $.get(getStringPageLoad, function(data){
 
-                   //
-                     $.get(getString, function(data){
-                       let characters3 = data.data.results[0].name;
-                       let character_path = data.data.results[0].thumbnail.path;
-                       let character_extension = data.data.results[0].thumbnail.extension
-                       let image_request = character_path + "." + character_extension;
-                       console.log(characters3);
-                       console.log(character_extension);
-                      $("#characters").append(characters3);
-                       $('#hero_img').attr('src',image_request);
-                      character_id = data.data.results[0].id;
-                    //  character_id = 1009351;
-                      console.log(character_id);
+           for(let i=0; i<100; i++){
+           loadCharactersName = data.data.results[i].name;
+           loadCharactersPath = data.data.results[i].thumbnail.path;
+           loadCharactersExtension = data.data.results[i].thumbnail.extension
+           let image_request = loadCharactersPath + "." + loadCharactersExtension;
+           console.log(loadCharactersName);
+           console.log(loadCharactersPath);
+           console.log(image_request)
 
-                    })
+           $("#charactersTable").append("<tr><td>" + loadCharactersName + "</td></tr>" );
+           $("#charactersTable").append("<tr><td><img src=\"" + image_request + "\" height=\"100px\"></img></td></tr>");
+            }
 
-                    //get events
+          })
 
-                  //  character_id = 1009351;
-                    getEventsString1 = "http://gateway.marvel.com/v1/public/characters/"
-                    getEventsString2 = "/events?ts=1&apikey=8e8d27528df8e1068e2773e3a7161575&hash=412852e32d04be7d8218808df40d53f1"
-                    getEventsStringFinal = getEventsString1 + character_id + getEventsString2;
-                    console.log("outside" + character_id);
-                    console.log(getEventsStringFinal);
-                    $.get(getEventsStringFinal, function(data){
+      }
 
-                      let events;
-                      let eventsDescription;
-                      for(let i = 0; i < 5; i++){
-                      eventsName = data.data.results[i].title;
-                      eventsDescription = data.data.results[i].description;
+      initialLoadCharacters()
 
+      function singleCharacters(){
 
-                      $("#events_title").append("Name: " + eventsName)
-                      $("#events_description").append("Description: " + eventsDescription);
+      let inputNameStartsWith = $('#search').val();
+        console.log("input:" + inputNameStartsWith);
+            getString = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=8e8d27528df8e1068e2773e3a7161575&hash=412852e32d04be7d8218808df40d53f1&nameStartsWith="+inputNameStartsWith;
+      $("#charactersTable").html("");
+        $.get(getString, function(data2){
 
-                    }
+           for(let i=0; i<20; i++){
+           searchCharactersName = data2.data.results[i].name;
+           searchCharactersPath = data2.data.results[i].thumbnail.path;
+           searchCharactersExtension = data2.data.results[i].thumbnail.extension
+           let search_image_request = searchCharactersPath + "." + searchCharactersExtension;
+           console.log(searchCharactersName);
+           console.log(searchCharactersPath);
+           console.log(search_image_request);
 
+           $("#charactersTable").append("<tr><td>" + searchCharactersName + "</td></tr>" );
+           $("#charactersTable").append("<tr><td><img src=\"" + search_image_request + "\" height=\"100px\"></img></td></tr>");
+          }
 
-                   })
+      })
 
-                    //end
+      }
 
+      //click functionality
+      $("#find").click(function(){
+        let inputNameStartsWith2 = $('#search').val();
+          if (inputNameStartsWith2 != ""){
+          singleCharacters();
+          }
+          else
+          {
 
-
-                })
-
+          initialLoadCharacters();
+          }
 
         })
-    })()
+
+  })
+})()
+
+    /*
+                        //get events
+                      //  character_id = 1009351;
+                        getEventsString1 = "http://gateway.marvel.com/v1/public/characters/"
+                        getEventsString2 = "/events?ts=1&apikey=8e8d27528df8e1068e2773e3a7161575&hash=412852e32d04be7d8218808df40d53f1"
+                        getEventsStringFinal = getEventsString1 + character_id + getEventsString2;
+                        console.log("outside" + character_id);
+                        console.log(getEventsStringFinal);
+                        $.get(getEventsStringFinal, function(data){
+
+                          let events;
+                          let eventsDescription;
+                          for(let i = 0; i < 5; i++){
+                          eventsName = data.data.results[i].title;
+                          eventsDescription = data.data.results[i].description;
+
+
+                          $("#events_title").append("Name: " + eventsName)
+                          $("#events_description").append("Description: " + eventsDescription);
+
+                        }
+
+
+                       })
+
+                        //end
+    */
